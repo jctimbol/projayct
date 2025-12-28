42e7d24d7ce517bfe4b58a240480e55ba32c3057
Jay Timbol <jlc.timbol@gmail.com>
2025-12-28 00:18:35 -0600

    title + subtitle

---
+++ components/sections/HeroSection.tsx
@@ -0,0 +1,10 @@
+export default function HeroSection() {
+  return (
+    <section className="hero">
+      <div className="heroStack">
+        <h1 className="title">jay timbol</h1>
+        <h2 className="subtitle">under construction...</h2>
+      </div>
+    </section>
+  );
+}
