298adb01cdb195d9faa9133835e030cd5d0757b9
Jay Timbol <jlc.timbol@gmail.com>
2025-12-29 00:58:29 -0600

    placeholder about section

--- components/sections/AboutSection.tsx
+++ components/sections/AboutSection.tsx
@@ -1,10 +1,19 @@
 export default function AboutSection() {
   return (
     <section className="about">
-      <h2 className="aboutTitle">about me</h2>
-      <p className="aboutText">
-        hello lalalalala sample text lalalalala i love coding
-      </p>
+      <div className="aboutGrid">
+        <div className="aboutLeft">
+          <h2 className="aboutTitle">about me</h2>
+          <p className="aboutText">
+            hello lalalalala sample text lalalalala i love coding
+            lalalalalalalalala i hate css lalalalala
+          </p>
+        </div>
+
+        <div className="aboutRight">
+          <img src="/stickers/nonclickable/matcha.png"></img>
+        </div>
+      </div>
     </section>
   );
 }
