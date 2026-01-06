b56116048f0d5e350ad114e22e9c98b96d588d5e
Jay Timbol <jlc.timbol@gmail.com>
2026-01-05 19:46:15 -0800

    start project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/sections/HeroSection";
 import AboutSection from "@/components/sections/AboutSection";
+import ProjectSection from "@/components/sections/ProjectSection";
 //red: #74070E
 export default function Home() {
   return (
     <main className="site">
       <HeroSection />
       <AboutSection />
+      <ProjectSection />
     </main>
   );
 }
---
+++ components/sections/ProjectSection.tsx
@@ -0,0 +1,13 @@
+export default function ProjectSection() {
+  return (
+    <section className="project">
+      <div className="projectTitle">recent projects</div>
+      <div className="projectGrid">
+        <div className="projectCard">seene</div>
+        <div className="projectCard">transit chatbot</div>
+        <div className="projectCard">ddd</div>
+        <div className="projectCard">hello stranger</div>
+      </div>
+    </section>
+  );
+}
